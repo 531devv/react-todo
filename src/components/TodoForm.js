5fe42aaaec4c970a4d45c79f52bdf2d145d9e1a5
Marek <531devv@gmail.com>
2022-05-21 17:21:10 +0200

    Adding todo components

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,39 +1,41 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 function TodoForm(props) {
-const [input, setInput] = useState('');
-const [id, setId] = useState(0);
+  const [input, setInput] = useState("");
+  const [id, setId] = useState(10);
 
-const handleChange = (e) => {
+  const handleChange = (e) => {
     setInput(e.target.value);
-}
+  };
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    setId(id + 1);
+    setId(id + 2);
 
     props.onSubmit({
-        id: id,
-        text: input
+      id: id,
+      text: input,
     });
 
-    setInput('');
-}
+    setInput("");
+  };
 
   return (
-      <form className="todo-form" onSubmit={handleSubmit}>
-          <input 
-            type="text" 
-            placeholder="Add a todo" 
-            value={input} 
-            name="text" 
-            className="todo-input"
-            onChange={handleChange}
-          />
-          <button className="todo-button">Add todo</button>
-      </form>
-  )
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+      />
+      <button type="submit" className="todo-button">
+        Add todo
+      </button>
+    </form>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,26 +1,50 @@
-import React, {useState} from 'react'
-import TodoForm from './TodoForm';
+import React, { useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
 
 function TodoList() {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = (todo) => {
-        if(!todo.text || /^\s*$/.test(todo.text)) {
-            return
-        }
+  const checkIsEmptyOrOnlySpaces = (text) => /^\s*$/.test(text);
 
-        const newTodos = [todo, ...todos];
-
-        setTodos(newTodos);
-        console.log(...todos)
+  const addTodo = (todo) => {
+    if (!todo.text || checkIsEmptyOrOnlySpaces(todo.text)) {
+      return;
     }
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || checkIsEmptyOrOnlySpaces(newValue)) {
+        return;
+      }
+    
+      setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  }
+
+  const removeTodo = (id) => {
+      const removeArr = [...todos].filter(todo => todo.id !== id);
+      setTodos(removeArr);
+  }
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
     <div>
-        <h1>What's the plan?</h1>
-        <TodoForm onSubmit={addTodo} />
+      <h1>What's the plan?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
     </div>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
